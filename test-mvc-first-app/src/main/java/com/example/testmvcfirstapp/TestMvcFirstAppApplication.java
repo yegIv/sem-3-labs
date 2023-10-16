ebec5c67d1455a8029352a45976754a4df0d8f24
user <>
2023-10-16 21:51:04 +0500

    Lab 5

---
+++ test-mvc-first-app/src/main/java/com/example/testmvcfirstapp/TestMvcFirstAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.testmvcfirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMvcFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMvcFirstAppApplication.class, args);
+	}
+
+}
