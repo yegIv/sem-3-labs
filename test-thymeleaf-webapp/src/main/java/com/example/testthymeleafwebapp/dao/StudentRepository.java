797d6fc46c3d29febadfb7d5a40115d4d11efe93
user <>
2023-10-16 22:32:25 +0500

    Lab 6

---
+++ test-thymeleaf-webapp/src/main/java/com/example/testthymeleafwebapp/dao/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.example.testthymeleafwebapp.dao;
+
+
+import com.example.testthymeleafwebapp.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
